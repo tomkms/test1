9c26e0f7d59dba449dbfb716bb2aded5b755272d
Tom Kuschel <tom.kuschel@mindsquare.de>
2022-11-08 18:30:41 +0100

    msg

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const http = require('http');
 const app = express();
 
 http.createServer(app).listen(3000);
 
 app.get('/', (req, res) => {
 res.send('hi kids');
-res.send('<a href="localhost:3000/route1">lol</a>');
+res.send();
 });
 
 app.get('/route1', (req, res) => {
